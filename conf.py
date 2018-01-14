fbfb073be56696abed30603faf43cc0d27b6294d
Spotlight <awesomebing1@gmail.com>
2018-01-13 21:10:24 -0600

    Revert to RTD theme

--- conf.py
+++ conf.py
@@ -33,9 +33,6 @@
 # ones.
 extensions = []
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
@@ -83,7 +80,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -164,6 +161,3 @@ texinfo_documents = [
      author, 'zenlikesmenAPI', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
